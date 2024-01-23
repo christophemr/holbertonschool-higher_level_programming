41198b483f3299543ce1b4e2aecfbfcc86096b64
christophemr <christophemr59@gmail.com>
2024-01-23 11:50:50 +0100

    pycodestyle fixes in 8 and 10 and typo fix in FizzBuzz

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in str:
         temp = i
         if ord(temp) >= 97 and ord(temp) <= 122:
-                temp = chr(ord(i) - 32)
+            temp = chr(ord(i) - 32)
         print("{}".format(temp), end='')
     print()
