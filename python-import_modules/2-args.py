fbc9ed5015c24a6e617b3e2ee319b55fa5ed9de4
christophemr <christophemr59@gmail.com>
2024-01-24 11:02:00 +0100

    program that prints the number and the list of its arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     args = len(argv) - 1
     if (args < 1):
-        print("0 arguments.")
+        print("{} arguments.".format(args))
     elif (args == 1):
-        print("1: argument:")
+        print("{}: argument:".format(args))
     else:
         print("{} arguments:".format(args))
     for i in range(args):
