f3a1968f9f1c268952fe7e2cf971f1af951ff7ea
christophemr <christophemr59@gmail.com>
2024-01-30 11:49:40 +0100

    removal of unwanted + in assignement

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -13,5 +13,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = tuple_b[0], 0
     # sum of corresponding elements of tuple_a and tuple_b
-    new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return (new_tuple)
