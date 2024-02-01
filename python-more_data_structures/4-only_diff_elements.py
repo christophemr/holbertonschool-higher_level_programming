131b62fa1f9972b11c8b73f5a083146427730058
christophemr <christophemr59@gmail.com>
2024-02-01 09:54:15 +0100

    comment divided in 2(pycodestyle)

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # Use the '^' operator to find the symmetric difference (unique elements in each set)
+    # Use the '^' operator to find the symmetric difference
+    # (unique elements in each set)
     return (set_1 ^ set_2)
