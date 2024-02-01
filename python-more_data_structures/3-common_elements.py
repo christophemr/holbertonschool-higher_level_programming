74d64a25c3e5f632fe8a61f7a143119e1b9870d5
christophemr <christophemr59@gmail.com>
2024-02-01 09:56:50 +0100

    comment divided in 2(pycodestyle)

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    #  Use the '&' operator to find the intersection (common elements) between two sets
+    #  Use the '&' operator to find the intersection
+    # (common elements) between two sets
     return (set_1 & set_2)
