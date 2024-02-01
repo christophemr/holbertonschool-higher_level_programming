825261388f8b3d0c4d29e29b0a1bedc7261201d9
christophemr <christophemr59@gmail.com>
2024-02-01 20:38:20 +0100

    pycodestyle fix

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -6,6 +6,8 @@ def safe_print_integer(value):
         # If successful, return True to indicate successful printing
         return True
     except (ValueError, TypeError):
-       # If an exception (ValueError or TypeError) occurs during formatting or printing,
-       # catch the exception and return False to indicate unsuccessful printing
+        # If an exception (ValueError or TypeError) occurs
+        # during formatting or printing,
+        # catch the exception and return
+        # False to indicate unsuccessful printing
         return False
