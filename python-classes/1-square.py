6f7cd9b9f08949bd491115432b23754ec7bd7a28
christophemr <christophemr59@gmail.com>
2024-02-05 09:39:26 +0100

    adding the private instant attribute 'size' to the square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+        defines a square
+    """
+
+    def __init__(self, size):
+        """
+            initialize the attribute size
+            @size: represent the size of the square
+        """
+        self.__size = size
