534258bbd8dd316c96cc5eba7013f2c66a1cb001
christophemr <christophemr59@gmail.com>
2024-02-05 10:27:42 +0100

    Public instance method: area that calculates the area of the square

---
+++ python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+        defines a square class
+    """
+
+    def __init__(self, size=0):
+        """intializing the square class
+
+
+        Args:
+            size (int): represent the size of the square
+              Defaults to 0.
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+            measure the area
+            return: the current square area
+        """
+        return (self.__size ** 2)
