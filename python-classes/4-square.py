8d61e0b5faba3af28ea8e65df37c2c0cc1db1e11
christophemr <christophemr59@gmail.com>
2024-02-05 10:53:29 +0100

    implementation of the Setter and Getter methods

---
+++ python-classes/4-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+        defines a square class
+    """
+
+    def __init__(self, size=0):
+        """intializing the square class
+
+
+        Args:
+            size (int): represent the size of the square
+              Defaults to 0.
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method for retrieving the square size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter method for setting the square size
+
+        Args:
+            value (int): the new size
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+        """
+        if not isinstance(value, int):
+            raise TypeError("must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+            measure the area
+            return: the current square area
+        """
+        return (self.__size ** 2)
