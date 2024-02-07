16db0a39e24d08866ef3ee8c1ff8fa3086edf8d9
christophemr <christophemr59@gmail.com>
2024-02-07 19:07:22 +0100

    TypeError corrected

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -20,7 +20,7 @@ class Square:
             ValueError: if size is less than 0
         """
         if not isinstance(size, int):
-            raise TypeError("must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
