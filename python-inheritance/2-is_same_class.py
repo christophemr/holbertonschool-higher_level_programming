f520091a0b0bb5965e1bbb589f30fddcda2eeb17
christophemr <christophemr59@gmail.com>
2024-02-14 10:52:39 +0100

    == changed to 'is' in the return statement

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@ def is_same_class(obj, a_class):
     """Return true if object is an instance of the
     specified class, otherwise return false
     """
-    return (type(obj) == a_class)
+    return (type(obj) is a_class)
