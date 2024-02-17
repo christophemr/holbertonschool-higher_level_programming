db99d39066c4837a7db4aaf76d51606cea91e6dd
christophemr <christophemr59@gmail.com>
2024-02-17 19:54:44 +0100

    docstring added

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-"""this module creates an object from a JSON file"""
+"""This module creates an object from a JSON file"""
 
 import json
 
 
 def load_from_json_file(filename):
+    """Load data from a JSON file and return the
+    corresponding Python object.
+
+    Args:
+        filename (str): The path to the JSON file.
+
+    Returns:
+        object: The Python object created from the JSON data.
+    """
     with open(filename, "r") as file:
         return json.load(file)
