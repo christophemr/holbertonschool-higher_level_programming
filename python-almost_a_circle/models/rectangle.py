2df6c3076c3dddb7c2bd1b431ecc4c0979d962fa
christophemr <christophemr59@gmail.com>
2024-02-21 10:08:26 +0100

    pycodestyle fix(documentation)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -38,7 +38,7 @@ class Rectangle(Base):
         """sets the value for width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -47,7 +47,7 @@ class Rectangle(Base):
         """sets the value for height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
