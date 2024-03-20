7def5b241f2d93e989b19aecb3f5fe4cc8f3fa18
christophemr <christophemr59@gmail.com>
2024-03-20 10:07:22 +0100

    completely revised code

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,12 +4,14 @@ This script defines a State class and
 a Base class to work with MySQLAlchemy ORM.
 """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 from sys import argv
 
+# Create a Base instance
 Base = declarative_base()
+
+# Define the State class
 
 
 class State(Base):
@@ -22,16 +24,17 @@ class State(Base):
     """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-    # Create connection string using command-line arguments
-    connect_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
 
-    # Connect to MySQL server
-    engine = create_engine(connect_url)
+# Create connection string using command-line arguments
+connect_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+    argv[1], argv[2], argv[3])
 
-    # Import all classes inheriting from Base before
-    # calling Base.metadata.create_all(engine)
-    Base.metadata.create_all(engine)
+# Create engine
+engine = create_engine(connect_url)
+
+# Import all classes inheriting from Base before
+# calling Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
