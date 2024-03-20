abe213b60c0b2e6b3e62e5d1cb68291dfc2ba395
christophemr <christophemr59@gmail.com>
2024-03-20 10:35:02 +0100

    fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,12 +6,9 @@ a Base class to work with MySQLAlchemy ORM.
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sys import argv
 
 # Create a Base instance
 Base = declarative_base()
-
-# Define the State class
 
 
 class State(Base):
@@ -26,15 +23,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-# Create connection string using command-line arguments
-connect_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-    argv[1], argv[2], argv[3])
-
-# Create engine
-engine = create_engine(connect_url)
-
-# Import all classes inheriting from Base before
-# calling Base.metadata.create_all(engine)
-Base.metadata.create_all(engine)
