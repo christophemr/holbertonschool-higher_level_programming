5b04fff727f91d191f70b98ff4c518e25ac37375
christophemr <christophemr59@gmail.com>
2024-03-22 09:45:44 +0100

    pycodestyle fix (line too long)

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that prints that adds the State object Louisiana to the database `hbtn_0e_6_usa`.
+script that prints that adds the State object
+Louisiana to the database `hbtn_0e_6_usa`.
 """
 
 from sys import argv
